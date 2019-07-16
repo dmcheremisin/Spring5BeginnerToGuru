326a0388ffc0c567772d91ee16f8882a0620857b
dmitrii89 <dmitry_cher@mail.ru>
2019-07-16 21:00:32 +0300

    Create publisher assignment

--- spring5webapp/src/main/java/info/cheremisin/spring5webapp/bootstrap/DevBootstrap.java
+++ spring5webapp/src/main/java/info/cheremisin/spring5webapp/bootstrap/DevBootstrap.java
@@ -2,8 +2,10 @@ package info.cheremisin.spring5webapp.bootstrap;
 
 import info.cheremisin.spring5webapp.model.Author;
 import info.cheremisin.spring5webapp.model.Book;
+import info.cheremisin.spring5webapp.model.Publisher;
 import info.cheremisin.spring5webapp.repositories.AuthorRepository;
 import info.cheremisin.spring5webapp.repositories.BookRepository;
+import info.cheremisin.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -13,10 +15,12 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -26,20 +30,24 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
 
     private void initData() {
         Author jeanned = new Author("Jeanned", "Boyarsky");
-        Book oca = new Book("Oracle Certified Associate Java SE 8", "12323", "Sybex");
-        Book ocp = new Book("Oracle Certified Professional Java SE 8", "12323", "Sybex");
+        Publisher sybex = new Publisher("Sybex", "111 River Street Hoboken, NJ 07030-5774");
+        Book oca = new Book("Oracle Certified Associate Java SE 8", "12323", sybex);
+        Book ocp = new Book("Oracle Certified Professional Java SE 8", "12323", sybex);
         jeanned.getBooks().add(oca);
         jeanned.getBooks().add(ocp);
         oca.getAuthors().add(jeanned);
         ocp.getAuthors().add(jeanned);
 
         Author iuliana = new Author("Iuliana", "Cosmina");
-        Book spring = new Book("Pivotal Certified Spring Web Application Developer Exam", "53454", "Apress");
+        Publisher apress = new Publisher("Apress", "233 Spring Street, 6th Floor New York, NY 100134");
+        Book spring = new Book("Pivotal Certified Spring Web Application Developer Exam", "53454", apress);
         iuliana.getBooks().add(spring);
         spring.getAuthors().add(iuliana);
 
         authorRepository.save(jeanned);
         authorRepository.save(iuliana);
+        publisherRepository.save(sybex);
+        publisherRepository.save(apress);
 
         bookRepository.save(oca);
         bookRepository.save(ocp);
