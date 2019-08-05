27e5d65d9bb1acc8f7359594005e93741f1887da
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-08-06 00:09:37 +0300

    domain model

---
+++ recipe-app/src/main/java/info/cheremisin/recipeapp/domain/Category.java
@@ -0,0 +1,48 @@
+package info.cheremisin.recipeapp.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import java.util.Set;
+
+/**
+ * Created by Dmitrii on 05.08.2019.
+ */
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set<Recipe> getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Set<Recipe> recipes) {
+        this.recipes = recipes;
+    }
+}
---
+++ recipe-app/src/main/java/info/cheremisin/recipeapp/domain/Difficulty.java
@@ -0,0 +1,10 @@
+package info.cheremisin.recipeapp.domain;
+
+/**
+ * Created by Dmitrii on 05.08.2019.
+ */
+
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
