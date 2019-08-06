aacbf704b73e71c01903bab6a0d1a647a9e997dc
dmitrii89 <dmitry_cher@mail.ru>
2019-08-06 13:36:18 +0300

    database initialization with spring

---
+++ recipe-app/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO UNIT_OF_MEASURE(description) VALUES ('teaspoon');
+INSERT INTO UNIT_OF_MEASURE(description) VALUES ('tablespoon');
+INSERT INTO UNIT_OF_MEASURE(description) VALUES ('cup');
+INSERT INTO UNIT_OF_MEASURE(description) VALUES ('pinch');
+INSERT INTO UNIT_OF_MEASURE(description) VALUES ('ounce');
+INSERT INTO CATEGORY(description) VALUES ('Russian');
+INSERT INTO CATEGORY(description) VALUES ('Italian');
+INSERT INTO CATEGORY(description) VALUES ('Mexican');
+INSERT INTO CATEGORY(description) VALUES ('European');
+INSERT INTO CATEGORY(description) VALUES ('Fast Food');
