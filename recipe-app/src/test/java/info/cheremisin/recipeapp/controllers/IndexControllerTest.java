76219ee90dc90480c58476f2643862dae3392b28
dmch0916 <$isNumber1>
2019-08-11 23:19:30 +0300

    Index controller test

---
+++ recipe-app/src/test/java/info/cheremisin/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,41 @@
+package info.cheremisin.recipeapp.controllers;
+
+import info.cheremisin.recipeapp.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
+
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    Model model;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+        String indexPage = indexController.getIndexPage(model);
+
+        assertEquals("index", indexPage);
+
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
