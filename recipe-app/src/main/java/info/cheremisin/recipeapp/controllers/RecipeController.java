ca1899f99b29af1b06f78808e9665d2208df172e
dmch0916 <$isNumber1>
2019-08-18 18:55:31 +0300

    Number Format Exception handling

--- recipe-app/src/main/java/info/cheremisin/recipeapp/controllers/RecipeController.java
+++ recipe-app/src/main/java/info/cheremisin/recipeapp/controllers/RecipeController.java
@@ -55,8 +55,21 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView exceptionHandler(Exception exception) {
-        log.error("Handling not found exception");
+    public ModelAndView notFoundExceptionHandler(Exception exception) {
+        log.error("Handling Not Found Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView numberFormatExceptionHandler(Exception exception) {
+        log.error("Handling Number Format Exception");
         log.error(exception.getMessage());
 
         ModelAndView modelAndView = new ModelAndView();
--- recipe-app/src/test/java/info/cheremisin/recipeapp/controllers/RecipeControllerTest.java
+++ recipe-app/src/test/java/info/cheremisin/recipeapp/controllers/RecipeControllerTest.java
@@ -105,6 +105,13 @@ public class RecipeControllerTest {
         mockMvc.perform(get("/recipe/4/show"))
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
+    }
+
+    @Test
+    public void testNumberFormatException() throws Exception {
+        mockMvc.perform(get("/recipe/sdfds/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("404error"));
 
     }
 }
