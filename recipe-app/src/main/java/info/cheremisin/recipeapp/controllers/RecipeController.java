ce5fff0f6913705c70ac7e7e1b5ee8dcf354c569
dmch0916 <$isNumber1>
2019-08-18 18:42:00 +0300

    Showing error data on the error page

--- recipe-app/src/main/java/info/cheremisin/recipeapp/controllers/RecipeController.java
+++ recipe-app/src/main/java/info/cheremisin/recipeapp/controllers/RecipeController.java
@@ -55,11 +55,13 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView exceptionHandler() {
+    public ModelAndView exceptionHandler(Exception exception) {
         log.error("Handling not found exception");
+        log.error(exception.getMessage());
 
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
 
         return modelAndView;
     }
