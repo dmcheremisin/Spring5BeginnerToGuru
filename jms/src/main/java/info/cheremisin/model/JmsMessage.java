6b8511d80f95b01c4a8299856ad971e76d591843
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2020-04-22 18:13:49 +0300

    jms template

---
+++ jms/src/main/java/info/cheremisin/model/JmsMessage.java
@@ -0,0 +1,19 @@
+package info.cheremisin.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class JmsMessage implements Serializable {
+
+    public static final long serialVersionUID = 2786966509880610246L;
+
+    private UUID id;
+    private String message;
+}
---
+++ jms/src/main/java/info/cheremisin/sender/JmsSender.java
@@ -0,0 +1,33 @@
+package info.cheremisin.sender;
+
+import info.cheremisin.model.JmsMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+import static info.cheremisin.config.JmsConfig.MY_QUEUE;
+
+@Component
+@RequiredArgsConstructor
+public class JmsSender {
+
+    private final JmsTemplate jmsTemplate;
+
+    @Scheduled(fixedRate = 2000)
+    public void sendMessage() {
+        System.out.println("I'm sending message");
+
+        JmsMessage message = JmsMessage.builder()
+                .id(UUID.randomUUID())
+                .message("Test message")
+                .build();
+
+        jmsTemplate.convertAndSend(MY_QUEUE, message);
+
+        System.out.println("Message was sent");
+
+    }
+}
